0aa0c5320bc1c95c19c9a28a7a3392d98a923430
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-12-18 13:29:01 +0545

    two view functions returning simple strings

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def Jan(request):
+    return HttpResponse("This is month January  ")    
+
+def Feb(request):
+    return HttpResponse("This is month Feburary")
