e14cc0fefffb0e8f19daaa6443f791aeb12b66a8
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-12-19 18:05:10 +0545

    error fix and try and except added

--- challenges/views.py
+++ challenges/views.py
@@ -21,9 +21,15 @@ monthly_challenges= {
 def monthly_challenge_by_number(request, month):
     months= list(monthly_challenges.keys())
     
+    if month > len (months):
+        return HttpResponseNotFound("invaild month")
+    
     redirect_month= months[month-1]
-    return HttpResponseRedirect("challenges/"+redirect_month)
+    return HttpResponseRedirect("/challenges/"+redirect_month)
 
-def monthly_challenge(request, month):
-    challenge_text= monthly_challenges[month]
-    return HttpResponse(challenge_text)
+def monthly_challenge(request, month): 
+    try:
+        challenge_text= monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("this month is not supported")
