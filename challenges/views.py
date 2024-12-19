5288ef3af1b9c9593efa67831d8ed616c0ffea29
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-12-19 14:37:51 +0545

    use Django's place holder for dynamic update

--- challenges/views.py
+++ challenges/views.py
@@ -2,21 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
-def Jan(request):
-    return HttpResponse("This is month January  ")    
 
-def Feb(request):
-    return HttpResponse("This is month Feburary")
+monthly_challenges= {
+    "january":"Learn a new language",
+    "febuary":"draw more",
+    "march":"read more books",
+    "april":"learn to fight",
+    "may":"learn music",
+    "june":"workout",
+    "july":"help someone",
+    "august":"save someone",
+    "september":"help random stranger",
+    "october":"learn to fly",
+    "november":"climb mount everst",
+    "december":"Learn Django",
+}
 
-def monthly_challenges(request, month):
-    challenge_text= None
-    if month == "January":
-        challenge_text = "Learn a new language"
-    elif month == "Febuary":
-        challenge_text = "Learn to read music"
-    elif month == "March":
-        challenge_text  = "Climb Mount Rushmore"
-    else:
-        return HttpResponseNotFound("This does not exist,404 error")
-    
+def monthly_challenge(request, month):
+    challenge_text= monthly_challenges[month]
     return HttpResponse(challenge_text)
